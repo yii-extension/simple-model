6697bbc86552d093117ec7b03321c7def2b2a54b
Wilmer <terabytesoftw@gmail.com>
2022-04-03 07:29:12 -0400

    Add `HasLengthRuleTest::class`.

---
+++ tests/Validator/Attribute/HasLengthRuleTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yii\Extension\FormModel\Tests\Validator\Attribute;
+
+use PHPUnit\Framework\TestCase;
+use Yii\Extension\FormModel\Attribute\FormErrorsAttributes;
+use Yii\Extension\FormModel\Tests\TestSupport\FormModel\Attribute\HasLengthRule;
+
+final class HasLengthRuleTest extends TestCase
+{
+    public function testAttribute(): void
+    {
+        $formModel = new HasLengthRule();
+        $this->assertFalse($formModel->validateWithAttributes());
+        $this->assertSame(
+            [
+                'name' => [
+                    'This value should contain at least {min, number} {min, plural, one{character} other{characters}}.'
+                ],
+            ],
+            FormErrorsAttributes::getAll($formModel)
+        );
+
+        $this->assertTrue($formModel->load(['HasLengthRule' => ['name' => 'al']]));
+        $this->assertFalse($formModel->validateWithAttributes());
+        $this->assertSame(
+            [
+                'name' => [
+                    'This value should contain at least {min, number} {min, plural, one{character} other{characters}}.'
+                ],
+            ],
+            FormErrorsAttributes::getAll($formModel)
+        );
+
+        $this->assertTrue($formModel->load(['HasLengthRule' => ['name' => 'samdark']]));
+        $this->assertFalse($formModel->validateWithAttributes());
+        $this->assertSame(
+            [
+                'name' => [
+                    'This value should contain at most {max, number} {max, plural, one{character} other{characters}}.'
+                ],
+            ],
+            FormErrorsAttributes::getAll($formModel)
+        );
+    }
+}
