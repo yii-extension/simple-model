aec21ace3ee34446d194e933b275588a60570c99
Wilmer <terabytesoftw@gmail.com>
2022-04-03 08:51:05 -0400

    Add `InRangeRuleTest::class`.

---
+++ tests/Validator/Attribute/InRangeRuleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yii\Extension\FormModel\Tests\Validator\Attribute;
+
+use PHPUnit\Framework\TestCase;
+use Yii\Extension\FormModel\Attribute\FormErrorsAttributes;
+use Yii\Extension\FormModel\Tests\TestSupport\FormModel\Attribute\InRangeRule;
+
+final class InRangeRuleTest extends TestCase
+{
+    public function testAttribute(): void
+    {
+        $formModel = new InRangeRule();
+        $this->assertFalse($formModel->validateWithAttributes());
+        $this->assertSame(['inRange' => ['This value is invalid.']], FormErrorsAttributes::getAll($formModel));
+
+        $this->assertTrue($formModel->load(['InRangeRule' => ['inRange' => '11']]));
+        $this->assertFalse($formModel->validateWithAttributes());
+        $this->assertSame(['inRange' => ['This value is invalid.']], FormErrorsAttributes::getAll($formModel));
+    }
+}
